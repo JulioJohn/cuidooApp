e9109b1008d8ec77e566188e5d7c29ce9fa73944
Victoria Andressa Faria <victoriaamdf@gmail.com>
2019-11-18 11:52:38 -0300

    Profile Storyboard done
    Finalizado o storyboard do perfil da mãa, com todas as constrains do modo portrait.

---
+++ profileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  profileViewController.swift
+//  CuidooApp
+//
+//  Created by Victoria Andressa S. M. Faria on 18/11/19.
+//  Copyright © 2019 Júlio John Tavares Ramos. All rights reserved.
+//
+
+import UIKit
+
+class profileViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    
+    
+    
+
+} // end class profileViewController
