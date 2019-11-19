ad1b1983806d953e209ae370d19b3625af0c9da3
Victor Toon <araujo.victor200@gmail.com>
2019-11-19 12:39:13 -0300

    Fiz a a BabySitterInfosView e o xib dela

---
+++ CuidooApp/Scenes/Request/BabySitterView/BabySitterInfosView.swift
@@ -0,0 +1,24 @@
+//
+//  BabySitterInfosView.swift
+//  CuidooApp
+//
+//  Created by Victor Toon de Araújo on 19/11/19.
+//  Copyright © 2019 Júlio John Tavares Ramos. All rights reserved.
+//
+
+import UIKit
+
+class BabySitterInfosView: UIView {
+
+    @IBOutlet weak var babySitterImage: UIImageView!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var descriptionText: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
