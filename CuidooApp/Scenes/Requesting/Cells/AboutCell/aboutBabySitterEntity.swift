4437dbc4b083f955609a79ae44717ec5f88f1272
Victoria Andressa Faria <victoriaamdf@gmail.com>
2019-11-27 11:11:14 -0300

    AboutCell done

---
+++ CuidooApp/Scenes/Requesting/Cells/AboutCell/aboutBabySitterEntity.swift
@@ -0,0 +1,15 @@
+//
+//  aboutBabySitterEntity.swift
+//  CuidooApp
+//
+//  Created by Victoria Andressa S. M. Faria on 27/11/19.
+//  Copyright © 2019 Júlio John Tavares Ramos. All rights reserved.
+//
+
+import Foundation
+
+struct aboutBabySitterEntity {
+    
+    var aboutBabySitter: String
+
+} // end struct aboutBabySitterEntity
