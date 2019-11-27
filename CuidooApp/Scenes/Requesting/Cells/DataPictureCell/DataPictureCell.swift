e2f181836a61fdd29bac424c3dc1251a46a628b0
Victoria Andressa Faria <victoriaamdf@gmail.com>
2019-11-27 09:43:38 -0300

    files created (with xibs) for screen call babySitter

---
+++ CuidooApp/Scenes/Requesting/Cells/DataPictureCell/DataPictureCell.swift
@@ -0,0 +1,24 @@
+//
+//  DataPictureCell.swift
+//  CuidooApp
+//
+//  Created by Victoria Andressa S. M. Faria on 27/11/19.
+//  Copyright © 2019 Júlio John Tavares Ramos. All rights reserved.
+//
+
+import UIKit
+
+class DataPictureCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ CuidooApp/Scenes/Requesting/Cells/RecomendationCell/RecomendationCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecomendationCell.swift
+//  CuidooApp
+//
+//  Created by Victoria Andressa S. M. Faria on 27/11/19.
+//  Copyright © 2019 Júlio John Tavares Ramos. All rights reserved.
+//
+
+import UIKit
+
+class RecomendationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
