4fa896ee70f8deb29ad872079d0b27b17170614b
Júlio John <julio_john@hotmail.com.br>
2019-12-07 22:55:48 -0300

    Corrigido visual das navegations, blue na request,

---
+++ CuidooApp/Extensions/NavegationBarExtension.swift
@@ -0,0 +1,9 @@
+//
+//  NavegationBarExtension.swift
+//  CuidooApp
+//
+//  Created by Júlio John Tavares Ramos on 07/12/19.
+//  Copyright © 2019 Júlio John Tavares Ramos. All rights reserved.
+//
+
+import Foundation
